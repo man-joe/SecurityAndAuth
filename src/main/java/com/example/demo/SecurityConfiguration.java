b16ec8c7bc2bba759f07b48c5dbde894771ab8c1
man-joe <jhoang3@gmail.com>
2019-10-15 10:29:45 -0400

    finished 4.06

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/","h2-console/**").permitAll()
+                .antMatchers("/","h2-console/**", "/register").permitAll()
 //                .access("hasAnyAuthority('USER','ADMIN')")
                 .antMatchers("/admin").access("hasAuthority('ADMIN')")
                 .anyRequest().authenticated()
